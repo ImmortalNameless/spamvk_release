9c9ddb69b48e1c8d449c0ff80bb83b3f6767b755
None_TM <pavelmarklev507@gmail.com>
2022-09-29 21:35:30 +0300

    updater to script

---
+++ updater.py
@@ -0,0 +1,24 @@
+import os
+
+print("Начинаю обновление...")
+
+files = os.listdir("./")
+ignore = ['message.txt', 'spamchats.txt', 'api.dbm.bak', 'api.dbm.dat', 'api.dbm.dir', 'api.dbm']
+saved = []
+
+for file in files:
+    if file not in ignore:
+        os.remove("./"+file)
+    else:
+        os.system(f"cp {file} ..")
+        saved.append(file)
+
+os.system("cd")
+os.rmdir("./spamvk_release")
+os.system("git clone https://github.com/None-TM/spamvk_release")
+for file in saved:
+    os.system(f"cp {file} ./spamvk_release")
+    os.remove("./"+file)
+os.chdir("./spamvk_release")
+
+print("Обновление завершено")
